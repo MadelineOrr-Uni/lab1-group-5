5fee6f69411fd5ec90e65d78a8922ecd30723036
Henry Radar <henry.radar@gmail.com>
2022-10-06 16:58:00 -0400

    Added variables

--- app/src/main/java/com/example/lab1_androidcalculator/MainActivity.java
+++ app/src/main/java/com/example/lab1_androidcalculator/MainActivity.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnSub, btnDiv, btnMul, btnClr, btnEql, btnDec;
+    TextView display;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = findByViewId(R.id.button_0);
     }
+
+
+
 }
